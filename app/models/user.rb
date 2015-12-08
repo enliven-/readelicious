11660c49f59f0827991a831276790576f39cb8d0
Viksit Arora <viksit@mit.edu>
2015-12-09 02:33:59 +0530

    sets up user authentication using bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,14 @@ class User < ActiveRecord::Base
   validates     :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates     :password, presence: true, length: { minimum: 6 }
 
   before_save   { self.email = email.downcase }
 
-  # has_secure_password
+  has_secure_password
 
   def name
-    self.first_name + ' ' + self.last_name
+    first_name + ' ' + last_name
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208182212) do
+ActiveRecord::Schema.define(version: 20151208202025) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(version: 20151208182212) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
