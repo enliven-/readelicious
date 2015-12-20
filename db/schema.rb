9f7dbb8d8876ff4eb584990bcf88f03e6ec3e5ad
Viksit Arora <viksit@mit.edu>
2015-12-20 17:15:51 +0530

    migrates tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220113748) do
+ActiveRecord::Schema.define(version: 20151220114518) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20151220113748) do
     t.integer  "status"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "reading_lists", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
